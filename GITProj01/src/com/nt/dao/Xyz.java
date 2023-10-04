7b7bc2a2a52a4d6e104a8cc2679c5bce9b9b98d9
af2al <afzal.hussain@sailssoftware.com>
2023-10-04 15:52:54 +0530

    TL--> modification

--- GITProj01/src/com/nt/dao/Xyz.java
+++ GITProj01/src/com/nt/dao/Xyz.java
@@ -3,7 +3,11 @@ package com.nt.dao;
 public class Xyz {
 	
 	public void m1() {
-		System.out.println("Xyz:m1()method");
+		System.out.println("Xyz:m1()method for learning");
+	
+		int a = 10;
+		int b = 20;
+		int c = a+b;
 	}
 
 }
